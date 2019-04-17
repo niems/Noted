f5e07737bb15caa2d3e7981b11a8d04a66ba654d
Zach Niemann <czniemann1990@gmail.com>
2019-04-17 13:16:06 -0500

    added fluid typography for noteview list. Currently removing react router

--- src/App.js
+++ src/App.js
@@ -1,77 +1,15 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Routes from './Routes';
+import NoteView from './components/NoteView';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      allNotes: [ //* stores all the saved notes
-        /*
-        {
-          id: this represents the current number of notes created; this.state.notesCreated++.
-          lastSaved: timestamp of the last time the note was saved
-
-          title (user input): title of the note.
-          note (user input): the saved note.
-        }
-        */
-        {
-          id: 1,
-          lastSaved: (new Date()).toUTCString(),
-          title: 'First saved note :D',
-          text: 'test note test note test note test note test note test note'
-        },
-        {
-          id: 2,
-          lastSaved: (new Date()).toUTCString(),
-          title: 'Another note title',
-          text: 'aanother note test data to see how the <li> wraps</li>another note test data to see how the <li> wraps</li>another note test data to see how the <li> wraps</li>nother note test data to see how the <li> wraps</li>'
-        },
-        {
-          id: 3,
-          lastSaved: (new Date()).toUTCString(),
-          title: '3 Another note title',
-          text: 'another note test data to see how the <li> wraps</li>'
-        },
-        {
-          id: 4,
-          lastSaved: (new Date()).toUTCString(),
-          title: 'adhfaAnother note title',
-          text: 'another note test data to see how the <li> wraps</li>'
-        },
-        {
-          id: 5,
-          lastSaved: (new Date()).toUTCString(),
-          title: ' ayyy Another note title',
-          text: 'another note test data to see how the <li> wraps</li>'
-        },
-      ], 
-
-      notesCreated: 0, //* total notes created - will differ from allNotes length if notes were deleted
-      isEditingNote: false, //* true if a note is being created / edited
-    };
-
-    this.addNote = this.addNote.bind(this); //* appends new note to allNotes
-    this.deleteNote = this.deleteNote.bind(this); //* deletes note based on id
-  }
-
-  addNote() {
-    return null;
-  }
-
-  deleteNote() {
-    return null;
-  }
-
   render() {
     return (
       <div className="App">
         <Header />
-        <Routes allNotes={this.state.allNotes} />
+        <NoteView />
         <Footer />
       </div>
     );
